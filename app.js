eeb249fed73d2a4760eab54b1034a22e82d8029c
Caden Lalley <cadenlalley@gmail.com>
2024-02-01 18:05:27 -0500

    Add docker-compose back to repo (lol)

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const app = express();
 
 // Configure App
 const host = process.env.HOST;
-const port = 8080;
-let url = "mongodb://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + "mongodb:" + process.env.DB_PORT + "/" + process.env.DB_NAME + "?authSource=admin";
+const port = process.env.PORT
+let url = process.env.DB_URI; 
 app.set('view engine', 'ejs');
 
 // Connect to MongoDB
